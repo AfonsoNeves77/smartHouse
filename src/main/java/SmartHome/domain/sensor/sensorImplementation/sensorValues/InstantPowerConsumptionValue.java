d5e8a55acd917926da00253efdb99f5ebab41d65
andre-s-rodrigues <1231823@isep.ipp.pt>
2024-03-05 14:56:49 +0000

    US24: Implemented InstantPowerConsumptionValue constructor and respective validation and  get methods plus test.

--- src/main/java/SmartHome/domain/sensor/sensorImplementation/sensorValues/InstantPowerConsumptionValue.java
+++ src/main/java/SmartHome/domain/sensor/sensorImplementation/sensorValues/InstantPowerConsumptionValue.java
@@ -1,4 +1,36 @@
 package SmartHome.domain.sensor.sensorImplementation.sensorValues;
 
-public class InstantPowerConsumptionValue {
+import org.apache.commons.lang3.ObjectUtils;
+
+import static java.lang.Double.parseDouble;
+
+public class InstantPowerConsumptionValue implements Value<Double>{
+    private final double primitiveValue;
+
+    public InstantPowerConsumptionValue(String reading) throws InstantiationException {
+        if(!isReadingValid(reading)){
+            throw new InstantiationException("Invalid reading");
+        }
+        this.primitiveValue = parseDouble(reading);
+    }
+
+    private boolean isReadingValid(String reading){
+        double valueOfReading;
+        try{
+            valueOfReading = parseDouble(reading);
+        } catch (NumberFormatException | NullPointerException e){
+            return false;
+        }
+        return valueOfReading >= 0;
+    }
+
+    @Override
+    public Double getValue() {
+        return this.primitiveValue;
+    }
+
+    @Override
+    public String getValueAsString() {
+        return this.primitiveValue + "";
+    }
 }
--- src/test/java/SmartHome/domainTest/sensorTest/sensorImplementationTest/sensorValuesTest/InstantPowerConsumptionValueTest.java
+++ src/test/java/SmartHome/domainTest/sensorTest/sensorImplementationTest/sensorValuesTest/InstantPowerConsumptionValueTest.java
@@ -1,4 +1,94 @@
 package SmartHome.domainTest.sensorTest.sensorImplementationTest.sensorValuesTest;
 
+import SmartHome.domain.sensor.sensorImplementation.sensorValues.InstantPowerConsumptionValue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class InstantPowerConsumptionValueTest {
+
+    @Test
+    void constructor_throwsInstantiationExceptionIfReadingNull(){
+        //Arrange
+        String reading = null;
+        String expected = "Invalid reading";
+
+        //Act
+        Exception exception = assertThrows(InstantiationException.class, () -> new InstantPowerConsumptionValue(reading));
+        String result = exception.getMessage();
+
+        //Assert
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void constructor_throwsInstantiationExceptionIfReadingEmpty() {
+        //Arrange
+        String reading = " ";
+        String expected = "Invalid reading";
+
+        //Act
+        Exception exception = assertThrows(InstantiationException.class, () -> new InstantPowerConsumptionValue(reading));
+        String result = exception.getMessage();
+
+        //Assert
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void constructor_throwsInstantiationExceptionIfReadingBelowBound(){
+        //Arrange
+        String reading = "-1.1";
+        String expected = "Invalid reading";
+
+        //Act
+        Exception exception = assertThrows(InstantiationException.class, () -> new InstantPowerConsumptionValue(reading));
+        String result = exception.getMessage();
+
+        //Assert
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void constructor_SuccessfulParseIfStringInt() throws InstantiationException {
+        //Arrange
+        String reading = "1";
+        double expected = 1.0;
+
+        //Act
+        InstantPowerConsumptionValue value = new InstantPowerConsumptionValue(reading);
+        double result = value.getValue();
+
+        //Assert
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void constructor_SuccessIfReadingLowerBound() throws InstantiationException {
+        //Arrange
+        String reading = "0.0";
+        double expected = 0.0;
+
+        //Act
+        InstantPowerConsumptionValue value = new InstantPowerConsumptionValue(reading);
+        double result = value.getValue();
+
+        //Assert
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void constructor_Success() throws InstantiationException {
+        //Arrange
+        String reading = "2323351.123412";
+        double expected = 2323351.123412;
+
+        //Act
+        InstantPowerConsumptionValue value = new InstantPowerConsumptionValue(reading);
+        double result = value.getValue();
+
+        //Assert
+        assertEquals(expected,result);
+    }
 }
