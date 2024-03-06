dfbb67481e59ae468e072a1cc71430f002b39a43
Hugo Sousa <1231830@isep.ipp.pt>
2024-03-06 19:33:21 +0000

    Added javadocs to ConfigureLocationCTRL class and tests.

--- src/main/java/SmartHome/controller/ConfigureLocationCTRL.java
+++ src/main/java/SmartHome/controller/ConfigureLocationCTRL.java
@@ -4,16 +4,27 @@ import SmartHome.domain.location.FactoryLocation;
 
 public class ConfigureLocationCTRL {
     private final House house;
-    private final FactoryLocation factoryLocation;
 
     /**
      * Constructor for the controller. It takes a house as parameter.
      */
-    public ConfigureLocationCTRL(House house, FactoryLocation factoryLocation) {
-        this.factoryLocation = factoryLocation;
+    public ConfigureLocationCTRL(House house) {
         this.house = house;
     }
 
+    /**
+     * Calls configure location on house, passing the address and gps creation parameters. It receives a boolean to indicate success.
+     * @param doorReference Door reference
+     * @param buildingNumber Building number
+     * @param streetName Street name
+     * @param city City
+     * @param country Country
+     * @param zipCode Zipcode
+     * @param latitude Latitude
+     * @param longitude Longitude
+     * @param factoryLocation Factory for location objects
+     * @return True or false
+     */
     public boolean configureLocation(String doorReference, String buildingNumber, String streetName, String city, String country, String zipCode, double latitude, double longitude, FactoryLocation factoryLocation) {
         return house.configureLocation(doorReference, buildingNumber, streetName, city, country, zipCode,latitude, longitude,factoryLocation);
     }
