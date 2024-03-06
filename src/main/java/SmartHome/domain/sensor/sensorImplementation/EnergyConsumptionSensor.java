05e10a1054821e47784807244c30b89438da8812
RicardoMPires <1231857@isep.ipp.pt>
2024-03-06 17:40:07 +0000

    Added type atribute and getType method and its respective tests.

--- src/main/java/SmartHome/domain/sensor/sensorImplementation/EnergyConsumptionSensor.java
+++ src/main/java/SmartHome/domain/sensor/sensorImplementation/EnergyConsumptionSensor.java
@@ -15,10 +15,16 @@ import java.util.ArrayList;
 public class EnergyConsumptionSensor implements Sensor{
     private String sensorName;
     private final String unit = "Wh";
+    private final String Type = "EnergyConsumption";
     private SimHardware simHardware;
 
     private final ArrayList<Value<Integer>> log = new ArrayList<>();
 
+    /**
+     * Constructor for EnergyConsumptionSensor
+     * @param sensorName name of the sensor
+     * @param externalServices external services
+     */
     public EnergyConsumptionSensor(String sensorName, ExternalServices externalServices) {
         if(!validateSensorName(sensorName)){
             throw new IllegalArgumentException("Invalid parameter");
@@ -27,10 +33,21 @@ public class EnergyConsumptionSensor implements Sensor{
         this.simHardware = (SimHardware) externalServices;
     }
 
+    /**
+     * Method to validate sensor name
+     * @param sensorName name of the sensor
+     */
     private boolean validateSensorName(String sensorName) {
         return sensorName != null && !sensorName.trim().isEmpty();
     }
 
+    /**
+     * Method to get the reading
+     * @param initialDate initial date
+     * @param finalDate final date
+     * @return reading value
+     * @throws InstantiationException
+     */
     public Value<Integer> getReading(String initialDate, String finalDate) throws InstantiationException {
         try {
             LocalDateTime initialDateTime = getInitialDateTime(initialDate);
@@ -50,32 +67,61 @@ public class EnergyConsumptionSensor implements Sensor{
     }
 
 
+    /**
+     * Method to get the initial date time
+     * @param initialDate initial date
+     * @return initial date time
+     */
     private LocalDateTime getInitialDateTime(String initialDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         LocalDateTime initialDateTime = LocalDateTime.parse(initialDate, formatter);
         return initialDateTime;
 
     }
+
+    /**
+     * Method to get the final date time
+     * @param finalDate final date
+     * @return final date time
+     */
     private LocalDateTime getFinalDateTime(String finalDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         LocalDateTime finalDateTime = LocalDateTime.parse(finalDate, formatter);
         return finalDateTime;
     }
 
+    /**
+     * Method to validate date
+     * @param initialDate initial date
+     * @param finalDateTime final date time
+     * @return
+     */
     private boolean validateDate(LocalDateTime initialDate, LocalDateTime finalDateTime) {
         return initialDate != null && finalDateTime != null && initialDate.isBefore(finalDateTime) && finalDateTime.isBefore(LocalDateTime.now());
     }
 
+    /**
+     * Method to get the name of the sensor
+     * @return name of the sensor
+     */
     @Override
     public String getName() {
         return this.sensorName;
     }
 
+    /**
+     * Method to get the unit of the sensor
+     * @return unit of the sensor
+     */
     @Override
     public String getUnit() {
         return this.unit;
     }
 
+    /**
+     * Method to get the log
+     * @return log
+     */
     @Override
     public ArrayList<String> getLog() {
         // 1.
@@ -87,7 +133,14 @@ public class EnergyConsumptionSensor implements Sensor{
         return tmpLog;
     }
 
+    public String getType() {
+        return Type;
+    }
 
+    /**
+     * Method to add value to log
+     * @param value value
+     */
     private void addValueToLog(Value<Integer> value){
         this.log.add(value);
     }
