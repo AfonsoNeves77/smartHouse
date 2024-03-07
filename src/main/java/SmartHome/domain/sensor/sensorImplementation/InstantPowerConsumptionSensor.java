eee1b61e830585915d02af8bc35f51d911198d11
andre-s-rodrigues <1231823@isep.ipp.pt>
2024-03-07 00:45:55 +0000

    US24: added javadocs to InstantPowerConsumptionSensor class.

--- src/main/java/SmartHome/domain/sensor/sensorImplementation/InstantPowerConsumptionSensor.java
+++ src/main/java/SmartHome/domain/sensor/sensorImplementation/InstantPowerConsumptionSensor.java
@@ -10,13 +10,17 @@ import java.util.ArrayList;
 
 
 public class InstantPowerConsumptionSensor implements Sensor {
-
     private String sensorName;
     private SimHardware simHardware;
     private final String unit = "W";
     private final String type = "InstantPowerConsumptionSensor";
     private final ArrayList<Value<Integer>> log = new ArrayList<>();
 
+    /**
+     * Constructor for Instant Power Consumption. The instantiation ensures the name is valid, throwing IllegalArgumentException otherwise.
+     * @param sensorName Name of the sensor
+     * @param externalServices External services to be used by the sensor cast to SimHardware
+     */
     public InstantPowerConsumptionSensor(String sensorName, ExternalServices externalServices){
         if(sensorName == null || sensorName.trim().isEmpty()){
             throw new IllegalArgumentException("Invalid parameter");
@@ -25,19 +29,36 @@ public class InstantPowerConsumptionSensor implements Sensor {
         this.simHardware = (SimHardware) externalServices;
     }
 
+    /**
+     * @return String containing the name of the sensor.
+     */
     @Override
     public String getName() {
         return this.sensorName;
     }
 
+    /**
+     * @return String containing the unit of the sensor.
+     */
     @Override
     public String getUnit() {
         return this.unit;
     }
+
+    /**
+     * @return String containing the type of the sensor.
+     */
     public String getTYPE() {
         return type;
     }
 
+    /**
+     * Communicates with simHardware in order to receive a reading, it then converts it to value,
+     * all the validations are done on value creation. It then adds that value object to the sensor's
+     * log and returns a copy of that value.
+     * @return Returns the reading obtained, validated and converted to Value.
+     * @throws InstantiationException If unable to create value.
+     */
     public Value<Integer> getReading() throws InstantiationException {
         String simValue = this.simHardware.getValue();
 
@@ -48,17 +69,28 @@ public class InstantPowerConsumptionSensor implements Sensor {
         return readingValue;
     }
 
+    /**
+     * Converts the log list from Value to String.
+     * 1. Creates an empty ArrayList of strings.
+     * 2. Iterates through all the Value objects saved into the log. For each Value object found, it converts
+     * it to string, and adds it to the ArrayList created in step 1. Returning it after it performs the iteration.
+     * @return Log as a list of strings.
+     */
     @Override
     public ArrayList<String> getLog() {
-
+        //1.
         ArrayList<String> tmpLog = new ArrayList<>();
-
+        //2.
         for (Value<Integer> value : this.log){
             tmpLog.add(value.getValueAsString());
         }
         return tmpLog;
     }
 
+    /**
+     * Receives a value generic of a specific wrapper class.
+     * @param value Value to be inserted.
+     */
     private void addValueToLog(Value<Integer> value) {
       this.log.add(value);
     }
