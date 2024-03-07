741c368a98abba67234219437fc4b3ce070bf2d5
Hugo Sousa <1231830@isep.ipp.pt>
2024-03-07 14:01:23 +0000

    AddRoom method - Added isolation test with extensive documentation

--- src/test/java/SmartHome/domainTest/HouseTest.java
+++ src/test/java/SmartHome/domainTest/HouseTest.java
@@ -10,12 +10,18 @@ import SmartHome.domain.location.FactoryLocation;
 import SmartHome.domain.location.GPS;
 import SmartHome.domain.room.FactoryIndoorRoom;
 import SmartHome.domain.room.FactoryRoom;
+import SmartHome.domain.room.ListOfRooms;
 import SmartHome.domain.room.Room;
 import SmartHome.domain.sensor.externalServices.SimHardware;
+import SmartHome.dto.DeviceDTO;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedConstruction;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mockConstruction;
 
 class HouseTest {
     @Test
@@ -112,6 +118,56 @@ class HouseTest {
         //Assert
         assertFalse(result);
     }
+
+    /**
+     * This test adds a room to the house's list of rooms object. The challenge in performing an isolation test is the
+     * fact that the ListOfRooms object is created on House instantiation.
+     * 1. Arranges the room parameters and factory room double.
+     * 2. This step creates a mocked construction of the object ListOfRooms. In order for this double to be used by the
+     * house, when the House is instantiated, the instantiation of House must be performed after this mocked construction.
+     * When this happens, the house, when instantiated, will use the double for ListOfRooms, instead of creating a new one.
+     * 3. After the house is instantiated, the mocked construction will automatically instantiate a listOfRoomsDouble. But
+     * now, to ensure we can create a stub for the ListOfRooms object's addRoom, we must access it. In order to access the
+     * object ListOfRoomsDouble, we must use the expression .constructed(), which will list all instances of the ListOfRooms
+     * double. To access the double that was created by instantiating the house, we must use get(0) and save that double into
+     * a variable (As opposite to a normal creation of a mock, the use of mocked constructions does not give us direct access
+     * to the created object). After saving the double into a variable, we then create a stub of its behaviour, for this
+     * test we want that method to return true.
+     * 4. Finally, considering the house is instantiated and has a double ListOfRooms object. We finally call addRoom,
+     * resulting into success.
+     * @throws InstantiationException If House/Room parameters invalid.
+     */
+    @Test
+    void addRoom_Success_Isolation() throws InstantiationException {
+        //Arrange
+        // 1.
+        String roomName = "Kitchen";
+        int floor = 0;
+        double roomWidth = 4.5;
+        double roomLength = 8.5;
+        double roomHeight = 2;
+        FactoryRoom factoryRoomDouble = mock(FactoryRoom.class);
+
+        // 2.
+        try (MockedConstruction<ListOfRooms> listOfRoomsMockedConstruction = mockConstruction(ListOfRooms.class)) {
+            House house = new House("House1");
+
+        // 3.
+            List<ListOfRooms> mockedListOfRoomsList = listOfRoomsMockedConstruction.constructed();
+            ListOfRooms listOfRoomsMocked = mockedListOfRoomsList.get(0);
+            when(listOfRoomsMocked.addRoomToList(roomName, floor, roomWidth, roomLength, roomHeight, factoryRoomDouble)).thenReturn(true);
+
+        // 4
+            // Act
+            boolean result = house.addRoom(roomName, floor, roomWidth, roomLength, roomHeight, factoryRoomDouble);
+
+            // Assert
+            assertTrue(result);
+        }
+    }
+
+    //////////////////////////////////////INTEGRATION///////////////////////////////////////////////////////////7
+
 
     @Test
     void addRoom_Success() throws InstantiationException {
