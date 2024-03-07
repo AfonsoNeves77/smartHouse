fb1657e864139bd02b5a63601a08d19a6479ccf7
RicardoMPires <1231857@isep.ipp.pt>
2024-03-07 16:03:57 +0000

    Added test to SimHardware.

--- src/test/java/SmartHome/domainTest/sensorTest/externalServicesTest/SimHardwareTest.java
+++ src/test/java/SmartHome/domainTest/sensorTest/externalServicesTest/SimHardwareTest.java
@@ -19,4 +19,18 @@ class SimHardwareTest {
         //Assert
         assertEquals(expected,result);
     }
+
+    /**
+     * This test ensures the getValue method returns the sample value successfully.
+     */
+    @Test
+    void successfullyReturnsSampleWithDates(){
+        //Arrange
+        SimHardware sim = new SimHardware();
+        String expected = "Sample";
+        //Act
+        String result = sim.getValue("15-12-2020 14:15:45", "16-12-2020 14:15:45");
+        //Assert
+        assertEquals(expected,result);
+    }
 }
