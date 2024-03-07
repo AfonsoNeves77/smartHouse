68cc43b367822a7a4a3ff8a196ef88a8c802ef98
andre-s-rodrigues <1231823@isep.ipp.pt>
2024-03-07 00:48:20 +0000

    US24: added javadocs to InstantPowerConsumptionValue and  InstantPowerConsumptionValueTest classes.

--- src/main/java/SmartHome/domain/sensor/sensorImplementation/sensorValues/InstantPowerConsumptionValue.java
+++ src/main/java/SmartHome/domain/sensor/sensorImplementation/sensorValues/InstantPowerConsumptionValue.java
@@ -5,6 +5,12 @@ import static java.lang.Integer.parseInt;
 public class InstantPowerConsumptionValue implements Value<Integer>{
     private final int primitiveValue;
 
+    /**
+     * Constructor for Instant Power Consumption Value. The instantiation ensures the reading is valid,
+     * throwing InstantiationException otherwise.
+     * @param reading String containing the reading to be converted to value.
+     * @throws InstantiationException If unable to create value.
+     */
     public InstantPowerConsumptionValue(String reading) throws InstantiationException {
         if(!isReadingValid(reading)){
             throw new InstantiationException("Invalid reading");
@@ -12,6 +18,12 @@ public class InstantPowerConsumptionValue implements Value<Integer>{
         this.primitiveValue = parseInt(reading);
     }
 
+    /**
+     * Validates the reading. Ensures the reading is not null or empty, that it is parseable, and when
+     * parsed, the instant power consumption value (in watts) is above 0.
+     * @param reading Reading to validate
+     * @return True or False
+     */
     private boolean isReadingValid(String reading){
         double valueOfReading;
         try{
@@ -22,11 +34,21 @@ public class InstantPowerConsumptionValue implements Value<Integer>{
         return valueOfReading >= 0;
     }
 
+    /**
+     * Returns the encapsulated primitive value, maintaining its type. Instant power consumption (in watts) is the
+     * primary value stored unto this value, so in order to utilize the Value interface, only this first int will be
+     * returned by getValue.
+     * @return Encapsulated primitive value.
+     */
     @Override
     public Integer getValue() {
         return this.primitiveValue;
     }
 
+    /**
+     * Returns the primitive value after conversion to String format.
+     * @return Encapsulated primitive value as String.
+     */
     @Override
     public String getValueAsString() {
         return this.primitiveValue + "";
