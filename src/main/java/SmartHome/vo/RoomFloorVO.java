71c4b844dc587939fa19b03037807067d6a791a9
Joao-Vieira-1231835 <1231835@isep.ipp.pt>
2024-03-18 21:39:26 +0000

    [feat]: add tests for RoomFloorVO

---
+++ src/main/java/SmartHome/vo/RoomFloorVO.java
@@ -0,0 +1,13 @@
+package SmartHome.vo;
+
+public class RoomFloorVO {
+    private int roomFloor;
+
+    public RoomFloorVO(int roomFloor) {
+        this.roomFloor = roomFloor;
+    }
+
+    public int getRoomFloor(){
+        return this.roomFloor;
+    }
+}
