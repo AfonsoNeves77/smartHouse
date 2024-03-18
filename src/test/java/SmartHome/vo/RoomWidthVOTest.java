721e34ef80ec55fef584221f034b389ba1154179
Joao-Vieira-1231835 <1231835@isep.ipp.pt>
2024-03-18 21:39:04 +0000

    [feat]: add tests for RoomWidthVO

---
+++ src/test/java/SmartHome/vo/RoomWidthVOTest.java
@@ -0,0 +1,43 @@
+package SmartHome.vo;
+
+import SmartHome.domain.room.RoomWidthDDD;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomWidthVOTest {
+
+    @Test
+    void getRoomWidth_Test(){
+        //Arrange
+        double width = 5;
+        //Act
+        RoomWidthVO roomWidth = new RoomWidthVO(width);
+        double result = roomWidth.getRoomWidth();
+        //Assert
+        assertEquals(width, result);
+    }
+    @Test
+    void roomWidth_objectCreation_FrontierSuccessTest(){
+        //Arrange
+        double width = 1;
+        //Act
+        RoomWidthVO roomWidth = new RoomWidthVO(width);
+        //Assert
+        assertNotNull(roomWidth);
+    }
+    @Test
+    void roomWidth_objectCreation_FrontierFailTest(){
+        // Arrange
+        double width = 0;
+        String expected = "Room Width has to be higher than zero";
+        // Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new RoomWidthVO(width);
+        });
+        String result = exception.getMessage();
+        // Assert
+        assertEquals(expected,result);
+    }
+
+}
