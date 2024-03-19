05b6f7ffbeb52dda774bfdfdda28f2ecd08f4a6c
RicardoMPires <1231857@isep.ipp.pt>
2024-03-19 09:23:07 +0000

    [feat]: add PostalCodeVO class and respective unitary testing. Fixes #23

---
+++ src/main/java/SmartHome/vo/PostalCodeVO.java
@@ -0,0 +1,17 @@
+package SmartHome.vo;
+
+public class PostalCodeVO {
+
+    private final String postalCode;
+
+    public PostalCodeVO(String postalCode) throws InstantiationException {
+        if (postalCode == null || postalCode.trim().isEmpty())
+            throw new InstantiationException("Please insert a valid country name.");
+        this.postalCode = postalCode;
+    }
+
+    public String getCountry() {
+        return postalCode;
+    }
+
+}
---
+++ src/test/java/SmartHome/voTest/PostalCodeVOTest.java
@@ -0,0 +1,35 @@
+package SmartHome.voTest;
+
+import SmartHome.vo.PostalCodeVO;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostalCodeVOTest {
+
+    @Test
+    void testValidParameter_returnsPostalCodeVO() throws InstantiationException {
+        String postalCode = "1234-567";
+        PostalCodeVO postalCodeVO = new PostalCodeVO(postalCode);
+        assertNotNull(postalCode);
+    }
+
+    @Test
+    void testNullParameter_throwsInstantiationException() {
+        String postalCode = null;
+        assertThrows(InstantiationException.class, () -> new PostalCodeVO(postalCode));
+    }
+
+    @Test
+    void testEmptyParameter_throwsInstantiationException() {
+        String postalCode = "";
+        assertThrows(InstantiationException.class, () -> new PostalCodeVO(postalCode));
+    }
+
+    @Test
+    void testGetPostalCode_returnsPostalCode() throws InstantiationException {
+        String postalCode = "Portugal";
+        PostalCodeVO postalCodeVO = new PostalCodeVO(postalCode);
+        assertEquals(postalCode, postalCodeVO.getCountry());
+    }
+}
