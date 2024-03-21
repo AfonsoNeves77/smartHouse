cd0141a172afb8bc33eb984187a3bbd134033afe
TiagoPires25 <1231862@isep.ipp.pt>
2024-03-21 17:20:47 +0000

    [docs]: added javadocs to RoomLengthVO class

--- src/main/java/SmartHome/vo/RoomLengthVO.java
+++ src/main/java/SmartHome/vo/RoomLengthVO.java
@@ -3,12 +3,25 @@ package SmartHome.vo;
 public class RoomLengthVO implements ValueObject{
     private final double length;
 
+    /**
+     * Constructs a RoomLengthVO object with the given length.
+     *
+     * @param length The length of the room.
+     * @throws IllegalArgumentException If the length is invalid.
+     */
     public RoomLengthVO(double length) {
         if (!validateLength(length)) {
             throw new IllegalArgumentException("Invalid length value");
         }
         this.length = length;
     }
+
+    /**
+     * Validates the length of the room.
+     *
+     * @param length The length of the room.
+     * @return True if the length is valid, false otherwise.
+     */
     private boolean validateLength(double length) {
         return length > 0;
     }
@@ -16,5 +29,4 @@ public class RoomLengthVO implements ValueObject{
     public double getLength() {
         return this.length;
     }
-
 }
