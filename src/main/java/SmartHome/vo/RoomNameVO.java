86d5c3e2975a1109d10250bac9088e49ab0b7e80
andre-s-rodrigues <1231823@isep.ipp.pt>
2024-03-21 14:57:27 +0000

    [docs]: add javadocs to LocationVO, LocationVOTest, RoomNameVO and RoomNameVOTest classes

--- src/main/java/SmartHome/vo/RoomNameVO.java
+++ src/main/java/SmartHome/vo/RoomNameVO.java
@@ -4,6 +4,11 @@ public class RoomNameVO implements ValueObject{
 
     private String roomName;
 
+    /** RoomNameVO constructor for the corresponding Value Object, with a String roomName as its parameter.
+     * If the String is either null, empty or blank it will throw an Illegal Argument Exception.
+     *
+     * @param roomName
+     */
     public RoomNameVO(String roomName) {
         if(!areParametersValid(roomName)) {
             throw new IllegalArgumentException("Invalid parameters.");
@@ -11,6 +16,12 @@ public class RoomNameVO implements ValueObject{
         this.roomName = roomName;
     }
 
+    /**
+     * Private method that verifies if roomName is null, empty or blank.
+     *
+     * @param roomName
+     * @return
+     */
     private boolean areParametersValid(String roomName) {
         if(roomName == null || roomName.trim().isEmpty() || roomName.isBlank()) {
             return false;
@@ -18,6 +29,10 @@ public class RoomNameVO implements ValueObject{
         return true;
     }
 
+    /**
+     * Public method that returns the room name.
+     * @return
+     */
     public String getRoomName() {
         return this.roomName;
     }
