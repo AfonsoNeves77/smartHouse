c2cb06e5478b81f1d2ec81f97c67fc11efdb7c9d
TiagoPires25 <1231862@isep.ipp.pt>
2024-03-21 14:51:55 +0000

    [docs]: added javadocs to RoomDimensionsVO class

--- src/main/java/SmartHome/vo/RoomDimensionsVO.java
+++ src/main/java/SmartHome/vo/RoomDimensionsVO.java
@@ -5,6 +5,14 @@ public class RoomDimensionsVO {
     private RoomWidthVO width;
     private RoomHeightVO height;
 
+    /**
+     * Constructs a RoomDimensionsVO object with the given length, width, and height.
+     *
+     * @param length The length of the room.
+     * @param width  The width of the room.
+     * @param height The height of the room.
+     * @throws IllegalArgumentException If the length, width, or height is null.
+     */
     public RoomDimensionsVO(RoomLengthVO length, RoomWidthVO width, RoomHeightVO height) {
         if (!dimensionsAreValid(length, width, height)) {
             throw new IllegalArgumentException("Invalid dimensions");
@@ -14,9 +22,18 @@ public class RoomDimensionsVO {
         this.height = height;
     }
 
+    /**
+     * Validates the length, width, and height objects.
+     *
+     * @param length The length of the room.
+     * @param width  The width of the room.
+     * @param height The height of the room.
+     * @return True if the length, width, and height are not null, otherwise false.
+     */
     private boolean dimensionsAreValid(RoomLengthVO length, RoomWidthVO width, RoomHeightVO height) {
         return length != null && width != null && height != null;
     }
+
 
     public double  getRoomLength() {
         return this.length.getLength();
