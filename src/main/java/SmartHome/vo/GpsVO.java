3031d6acaaaa26499f0b8fe3a34d9ef77393e489
Mariana Pereira <1231844@isep.ipp.pt>
2024-03-22 11:38:25 +0000

    [refactor]: change GpsVO Class attributes to final
    Co-authored-by: Carla Santos <1231825@isep.ipp.pt>

--- src/main/java/SmartHome/vo/GpsVO.java
+++ src/main/java/SmartHome/vo/GpsVO.java
@@ -1,9 +1,14 @@
 package SmartHome.vo;
 
 public class GpsVO implements ValueObject{
-    private LatitudeVO latitude;
-    private LongitudeVO longitude;
+    private final LatitudeVO latitude;
+    private final LongitudeVO longitude;
 
+    /**
+     * GPS Constructor for the corresponding Value Object composed by a Latitude and Longitude Value Objects
+     * @param latitude Latitude Value Object
+     * @param longitude Longitude Value Object
+     */
     public GpsVO(LatitudeVO latitude, LongitudeVO longitude){
         if(latitude == null || longitude == null)
             throw new IllegalArgumentException("Latitude and longitude cannot be null.");
