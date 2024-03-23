3d47fa0d65a9da08eab521f83c04980e6355f6a4
TiagoPires25 <1231862@isep.ipp.pt>
2024-03-23 14:55:53 +0000

    [feat]: added HouseIDVO and respective HouseIDVOTest class. Fixes #41

---
+++ src/main/java/SmartHome/ddd/HouseIDVO.java
@@ -0,0 +1,34 @@
+package SmartHome.ddd;
+
+import java.util.UUID;
+
+/**
+ * Represents a value object for House ID.
+ */
+public class HouseIDVO implements DomainID {
+
+    private UUID identifier;
+
+    /**
+     * Constructs a HouseIDVO object with the given identifier.
+     *
+     * @param identifier The UUID identifier for the house.
+     * @throws IllegalArgumentException If the identifier is null.
+     */
+    public HouseIDVO(UUID identifier){
+        if(identifier == null){
+            throw new IllegalArgumentException("Invalid Identifier");
+        }
+        this.identifier = identifier;
+    }
+
+    /**
+     * Gets the string representation of the house identifier.
+     *
+     * @return The string representation of the house identifier.
+     */
+    @Override
+    public String getID() {
+        return this.identifier.toString();
+    }
+}
---
+++ src/test/java/SmartHome/ddd/HouseIDVOTest.java
@@ -0,0 +1,52 @@
+package SmartHome.ddd;
+
+
+import SmartHome.ddd.HouseIDVO;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class HouseIDVOTest {
+
+    /**
+     * Test case to verify that the HouseIDVO constructor throws an IllegalArgumentException
+     * when given a null identifier. The result exception description must also match the expected one.
+     */
+    @Test
+    public void givenNullIdentifier_ShouldThrowIllegalArgumentException(){
+
+        //Arrange
+        String expected = "Invalid Identifier";
+
+        //Act
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> new HouseIDVO(null));
+        String result = exception.getMessage();
+
+        //Assert
+        assertEquals(expected,result);
+
+    }
+
+    /**
+     * Test case to verify that the getID method of HouseIDVO returns the correct ID as a string.
+     */
+    @Test
+    public void getIDShouldReturnCorrectIDAsString(){
+
+        //Arrange
+        UUID houseIdentifier = UUID.randomUUID();
+        String expected = houseIdentifier.toString();
+
+        //Act
+        HouseIDVO houseID = new HouseIDVO(houseIdentifier);
+        String result = houseID.getID();
+
+        //Assert
+        assertEquals(expected,result);
+
+    }
+}
