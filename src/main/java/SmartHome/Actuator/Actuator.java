359dc046cfa3dce5fd5280ca1770b8d6072ca368
José Castro <1231836@isep.ipp.pt>
2024-03-25 14:25:20 +0000

    [feat]: Add Actuator Interface and Javadocs. fixes #78

---
+++ src/main/java/SmartHome/Actuator/Actuator.java
@@ -0,0 +1,9 @@
+package SmartHome.Actuator;
+
+public interface Actuator {
+    /**
+     * This Interface has no methods yet. Each implementation of actuator will implement DomainEntity which will
+     * offer the ability to getID. The presence of this Interface is fundamentally rooted in the necessity to persist
+     * all actuators within an Actuator repository.
+     */
+}
