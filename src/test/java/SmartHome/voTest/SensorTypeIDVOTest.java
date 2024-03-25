cd35bd97d130313fa8e84d671ead432b9ed9fce9
RicardoMPires <1231857@isep.ipp.pt>
2024-03-25 12:17:01 +0000

    [feat]: add SensorTypeIDVO class and tests. Fixes #46

---
+++ src/test/java/SmartHome/voTest/SensorTypeIDVOTest.java
@@ -0,0 +1,32 @@
+package SmartHome.voTest;
+
+import SmartHome.vo.SensorTypeIDVO;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SensorTypeIDVOTest {
+
+    @Test
+    void testValidParameters_returnsSensorTypeIDVO() {
+        UUID sensorTypeID = UUID.randomUUID();
+        SensorTypeIDVO sensorTypeIDVO = new SensorTypeIDVO(sensorTypeID);
+        assertNotNull(sensorTypeIDVO);
+    }
+
+    @Test
+    void testNullSensorTypeID_throwsIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> new SensorTypeIDVO(null));
+    }
+
+
+    @Test
+    void testGetSensorTypeID_returnsSensorTypeID() {
+        UUID sensorTypeID = UUID.randomUUID();
+        String expected = sensorTypeID.toString();
+        SensorTypeIDVO sensorTypeIDVO = new SensorTypeIDVO(sensorTypeID);
+        assertEquals(expected, sensorTypeIDVO.getID());
+    }
+}
