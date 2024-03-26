d5ceb04a51346303d5e4f22499693b74bd685357
Hugo Sousa <1231830@isep.ipp.pt>
2024-03-26 23:05:52 +0000

    [feat]: added SensorRepository Class which contain the pre-specified methods, repository test class, and javadoc documentation. Modified sensor interface. It is now extending DomainEntity. Fixes #83

--- src/main/java/SmartHomeDDD/domain/sensor/Sensor.java
+++ src/main/java/SmartHomeDDD/domain/sensor/Sensor.java
@@ -1,6 +1,8 @@
 package SmartHomeDDD.domain.sensor;
 
-public interface Sensor {
+import SmartHomeDDD.domain.DomainEntity;
+
+public interface Sensor extends DomainEntity {
     /**
      * This Interface has no methods yet. Each implementation of sensor will implement DomainEntity which will
      * offer the ability to getID. The presence of this Interface is fundamentally rooted in the necessity to persist
