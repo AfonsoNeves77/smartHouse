7e96103c248141168f1f72856c39ff5a12ce6fa4
RicardoMPires <1231857@isep.ipp.pt>
2024-03-26 12:24:45 +0000

    [feat] add SimHardware class.

---
+++ src/main/java/SmartHomeDDD/domain/sensor/externalServices/SimHardware.java
@@ -0,0 +1,13 @@
+package SmartHomeDDD.domain.sensor.externalServices;
+
+import SmartHome.domain.sensor.externalServices.ExternalServices;
+
+public class SimHardware {
+    public String getValue() {
+        return "Sample";
+    }
+
+    public String getValue(String initialDate, String finalDate) {
+        return "Sample";
+    }
+}
