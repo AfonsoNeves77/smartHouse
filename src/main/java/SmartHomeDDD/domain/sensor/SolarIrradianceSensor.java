e0004dc8a25b9d252f5cb7352f4e6d3b6a28e98a
Mariana Pereira <1231844@isep.ipp.pt>
2024-04-04 17:44:33 +0100

    [feat]: add Class Diagram for US23. Fixes #119
    - Add .png file to archive Sprint4/US23 folder
    - Update SolarIrradianceSensor Class documentation in accordance
    - Remove old files

--- src/main/java/SmartHomeDDD/domain/sensor/SolarIrradianceSensor.java
+++ src/main/java/SmartHomeDDD/domain/sensor/SolarIrradianceSensor.java
@@ -22,9 +22,9 @@ public class SolarIrradianceSensor implements Sensor {
      * Constructor for SolarIrradianceSensor Class.
      * Starts by validating input parameter and proceeds with the creation of a unique sensor ID, using the Java's
      * built-in class named java.util.UUID.
-     * @param sensorName Sensor name
-     * @param deviceID Device ID where the sensor is going to be connected to
-     * @param sensorTypeID ID of the sensor type chosen
+     * @param sensorName Sensor name Value Object
+     * @param deviceID Device ID Value Object where the sensor is going to be connected to
+     * @param sensorTypeID ID Value Object of the sensor type chosen
      */
     public SolarIrradianceSensor(SensorNameVO sensorName, DeviceIDVO deviceID, SensorTypeIDVO sensorTypeID){
         if(!validParameters(sensorName, deviceID, sensorTypeID))
@@ -37,7 +37,7 @@ public class SolarIrradianceSensor implements Sensor {
 
     /**
      * Requests the sensor to retrieve its reading
-     * @return Value<Integer> containing the reading of the sensor
+     * @return ValueObject<Integer> containing the reading of the sensor
      * @throws IllegalArgumentException If reading hardware is null or reading is invalid (null or wrong number format)
      */
     public ValueObject<Integer> getReading(SimHardware simHardware) {
