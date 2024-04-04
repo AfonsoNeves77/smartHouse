41c8e78c9f5723d9e63f054c3897690c82561d2e
RicardoMPires <1231857@isep.ipp.pt>
2024-04-04 15:01:48 +0100

    [refactor] add createActuatorTypeIDVOFromString to ActuatorTypeMapper. Fixes #202

--- src/main/java/SmartHomeDDD/mapper/ActuatorTypeMapper.java
+++ src/main/java/SmartHomeDDD/mapper/ActuatorTypeMapper.java
@@ -12,14 +12,35 @@ public class ActuatorTypeMapper {
     private static List<ActuatorTypeDTO> actuatorTypeDTOList = new ArrayList<ActuatorTypeDTO>();
 
 
+    /**
+     * Maps a list of ActuatorType to a list of ActuatorTypeDTO
+     *
+     * @param actuatorTypeList list of ActuatorType
+     * @return list of ActuatorTypeDTO
+     */
     public static List<ActuatorTypeDTO> domainToDTO(List<ActuatorType> actuatorTypeList) {
         for (ActuatorType actuatorType : actuatorTypeList) {
-            actuatorTypeDTOList.add(new ActuatorTypeDTO(actuatorType.getActuatorType()));
+            actuatorTypeDTOList.add(new ActuatorTypeDTO(actuatorType.getId().getID()));
         }
         return actuatorTypeDTOList;
     }
 
-    public static ActuatorTypeIDVO createActuatorTypeIDVO(String actuatorType) {
+    /**
+     * Creates an ActuatorTypeIDVO from an ActuatorTypeDTO
+     *
+     * @param actuatorTypeDTO ActuatorTypeDTO
+     * @return ActuatorTypeIDVO
+     * @throws IllegalArgumentException if ActuatorTypeDTO is null
+     */
+    public static ActuatorTypeIDVO createActuatorTypeIDVO(ActuatorTypeDTO actuatorTypeDTO) {
+        if (actuatorTypeDTO == null) {
+            throw new IllegalArgumentException("ActuatorTypeDTO cannot be null");
+        }
+        String actuatorType = actuatorTypeDTO.getActuatorTypeID();
+        return new ActuatorTypeIDVO(actuatorType);
+    }
+
+    public static ActuatorTypeIDVO createActuatorTypeIDVOFromString(String actuatorType){
         return new ActuatorTypeIDVO(actuatorType);
     }
 }
--- src/test/java/SmartHomeDDDTest/mapperTest/ActuatorTypeMapperTest.java
+++ src/test/java/SmartHomeDDDTest/mapperTest/ActuatorTypeMapperTest.java
@@ -16,13 +16,19 @@ import static org.mockito.Mockito.when;
 class ActuatorTypeMapperTest {
 
     @Test
-    void domainToDTO() {
+    void givenValidActuators_ThenReturnDTO() {
         ActuatorType actuator1Double = mock(ActuatorType.class);
         ActuatorType actuator2Double = mock(ActuatorType.class);
         ActuatorType actuator3Double = mock(ActuatorType.class);
-        when(actuator1Double.getActuatorType()).thenReturn("actuator1");
-        when(actuator2Double.getActuatorType()).thenReturn("actuator2");
-        when(actuator3Double.getActuatorType()).thenReturn("actuator3");
+        ActuatorTypeIDVO actuatorTypeIDVO1 = mock(ActuatorTypeIDVO.class);
+        ActuatorTypeIDVO actuatorTypeIDVO2 = mock(ActuatorTypeIDVO.class);
+        ActuatorTypeIDVO actuatorTypeIDVO3 = mock(ActuatorTypeIDVO.class);
+        when(actuator1Double.getId()).thenReturn(actuatorTypeIDVO1);
+        when(actuator2Double.getId()).thenReturn(actuatorTypeIDVO2);
+        when(actuator3Double.getId()).thenReturn(actuatorTypeIDVO3);
+        when(actuatorTypeIDVO1.getID()).thenReturn("actuator1");
+        when(actuatorTypeIDVO2.getID()).thenReturn("actuator2");
+        when(actuatorTypeIDVO3.getID()).thenReturn("actuator3");
         List<ActuatorType> list = new ArrayList<>();
         list.add(actuator1Double);
         list.add(actuator2Double);
@@ -36,19 +42,25 @@ class ActuatorTypeMapperTest {
     }
 
     @Test
-    void createActuatorTypeIDVO() {
-        ActuatorType actuator1Double = mock(ActuatorType.class);
-        ActuatorType actuator2Double = mock(ActuatorType.class);
-        ActuatorType actuator3Double = mock(ActuatorType.class);
-        when(actuator1Double.getActuatorType()).thenReturn("actuator1");
-        when(actuator2Double.getActuatorType()).thenReturn("actuator2");
-        when(actuator3Double.getActuatorType()).thenReturn("actuator3");
-        String actuator1 = actuator1Double.getActuatorType();
-        String actuator2 = actuator2Double.getActuatorType();
-        String actuator3 = actuator3Double.getActuatorType();
-        ActuatorTypeIDVO actuatorTypeIDVO1 = ActuatorTypeMapper.createActuatorTypeIDVO(actuator1);
-        ActuatorTypeIDVO actuatorTypeIDVO2 = ActuatorTypeMapper.createActuatorTypeIDVO(actuator2);
-        ActuatorTypeIDVO actuatorTypeIDVO3 = ActuatorTypeMapper.createActuatorTypeIDVO(actuator3);
+    void givenNullActuatorTypeDTO_ThenThrowIllegalArgumentException() {
+        ActuatorTypeDTO actuatorDouble = null;
+        assertThrows(IllegalArgumentException.class, () -> ActuatorTypeMapper.createActuatorTypeIDVO(actuatorDouble));
+    }
+
+    @Test
+    void givenValidActuatorTypeDTO_ThenCreateActuatorTypeIDVO() {
+        ActuatorTypeDTO actuator1Double = mock(ActuatorTypeDTO.class);
+        ActuatorTypeDTO actuator2Double = mock(ActuatorTypeDTO.class);
+        ActuatorTypeDTO actuator3Double = mock(ActuatorTypeDTO.class);
+        when(actuator1Double.getActuatorTypeID()).thenReturn("actuator1");
+        when(actuator2Double.getActuatorTypeID()).thenReturn("actuator2");
+        when(actuator3Double.getActuatorTypeID()).thenReturn("actuator3");
+        String actuator1 = actuator1Double.getActuatorTypeID();
+        String actuator2 = actuator2Double.getActuatorTypeID();
+        String actuator3 = actuator3Double.getActuatorTypeID();
+        ActuatorTypeIDVO actuatorTypeIDVO1 = ActuatorTypeMapper.createActuatorTypeIDVO(actuator1Double);
+        ActuatorTypeIDVO actuatorTypeIDVO2 = ActuatorTypeMapper.createActuatorTypeIDVO(actuator2Double);
+        ActuatorTypeIDVO actuatorTypeIDVO3 = ActuatorTypeMapper.createActuatorTypeIDVO(actuator3Double);
         assertEquals("actuator1", actuatorTypeIDVO1.getID());
         assertEquals("actuator2", actuatorTypeIDVO2.getID());
         assertEquals("actuator3", actuatorTypeIDVO3.getID());
