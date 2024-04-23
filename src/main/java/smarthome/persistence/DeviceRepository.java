448cb52d996911db69d70f6e8d7fd943e5424dd2
MiguelMoura90 <1231848@isep.ipp.pt>
2024-04-23 14:29:04 +0100

    [feat] Add Update method to DeviceRepository interface. Fixes #349

--- src/main/java/smarthome/persistence/DeviceRepository.java
+++ src/main/java/smarthome/persistence/DeviceRepository.java
@@ -1,11 +1,14 @@
 package smarthome.persistence;
 
 import smarthome.domain.device.Device;
+import smarthome.domain.house.House;
 import smarthome.domain.vo.devicevo.DeviceIDVO;
 import smarthome.domain.vo.roomvo.RoomIDVO;
 
 import java.util.List;
 
 public interface DeviceRepository extends Repository<DeviceIDVO, Device>{
-    List<Device> findByRoomID(RoomIDVO roomID);
+    Iterable<Device> findByRoomID(RoomIDVO roomID);
+
+    boolean update(Device device);
 }
