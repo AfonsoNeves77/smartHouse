05e70b6fb1d7dd2725c55ea260f9fb67d4c9c415
Joao-Vieira-1231835 <1231835@isep.ipp.pt>
2024-04-26 00:58:06 +0100

    [feat] add method to log service interface to get maximum instantaneous temperature difference #373
    Co-authored-by: andre-s-rodrigues <1231823@isep.ipp.pt>

--- src/main/java/smarthome/service/LogService.java
+++ src/main/java/smarthome/service/LogService.java
@@ -6,6 +6,10 @@ import smarthome.utils.timeconfig.TimeConfig;
 
 import java.util.List;
 
+/**
+ * Service interface for handling log data.
+ */
 public interface LogService {
     List<Log> findReadingsFromDeviceInATimePeriod(DeviceIDVO deviceIDVO, TimeConfig timeConfig);
+    String getMaxInstantaneousTempDifference(DeviceIDVO outdoorDevice, DeviceIDVO indoorDevice, TimeConfig timeConfig);
 }
