649edb525a61d4fd6d319612e3bb47e5c5f0ac25
Carla Santos <1231825@isep.ipp.pt>
2024-05-18 17:51:55 +0100

    [fix]: update application.properties file. Fixes #441

--- src/main/java/smarthome/persistence/springdata/SpringDataConfig.java
+++ src/main/java/smarthome/persistence/springdata/SpringDataConfig.java
@@ -1,39 +1,39 @@
-package smarthome.persistence.springdata;
-
-
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
-
-
-@Configuration
-@EnableJpaRepositories(basePackages = "smarthome.persistence.springdata")
-@ComponentScan("smarthome.persistence.springdata")
-public class SpringDataConfig {
-
-    LocalContainerEntityManagerFactoryBean em;
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
-        em = new LocalContainerEntityManagerFactoryBean();
-        em.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
-        em.setPersistenceUnitName("smartHome");
-        em.setPackagesToScan("smarthome.persistence.jpa.datamodel");
-        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
-        return em;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
-
-}
+//package smarthome.persistence.springdata;
+//
+//
+//import jakarta.persistence.EntityManagerFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.orm.jpa.JpaTransactionManager;
+//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+//import org.springframework.transaction.PlatformTransactionManager;
+//
+//
+//@Configuration
+//@EnableJpaRepositories(basePackages = "smarthome.persistence.springdata")
+//@ComponentScan("smarthome.persistence.springdata")
+//public class SpringDataConfig {
+//
+//    LocalContainerEntityManagerFactoryBean em;
+//
+//    @Bean
+//    public LocalContainerEntityManagerFactoryBean entityManagerFactory(){
+//        em = new LocalContainerEntityManagerFactoryBean();
+//        em.setPersistenceXmlLocation("classpath:META-INF/persistence.xml");
+//        em.setPersistenceUnitName("smartHome");
+//        em.setPackagesToScan("smarthome.persistence.jpa.datamodel");
+//        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+//        return em;
+//    }
+//
+//    @Bean
+//    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory){
+//        JpaTransactionManager transactionManager = new JpaTransactionManager();
+//        transactionManager.setEntityManagerFactory(entityManagerFactory);
+//        return transactionManager;
+//    }
+//
+//}
