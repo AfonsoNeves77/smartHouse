5b75fb55fb772ed6225d474729ee0b8e576abf27
Carla Santos <1231825@isep.ipp.pt>
2024-06-13 15:14:20 +0100

    [refactor]: change add actuator pop up appearance. Fixes #533

--- frontend/smarthome/src/components/Popup.js
+++ frontend/smarthome/src/components/Popup.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import { Dialog, DialogTitle} from '@mui/material';
-import { styled } from '@mui/system';
+import {Snackbar} from '@mui/material';
 
-const CustomDialog = styled(Dialog)(({ theme }) => ({
-    '& .MuiDialogTitle-root': {
-        backgroundColor: theme.palette.primary.main,
-        color: theme.palette.primary.contrastText,
-        textAlign: 'center',
-        padding: theme.spacing(2),
-    },
-    '& .MuiDialogContent-root': {
-        padding: theme.spacing(2),
-    },
-}));
 
-const Popup = ({ open, onClose }) => {
+const Popup = ({open, onClose}) => {
     return (
-        <CustomDialog open={open} onClose={onClose}>
-            <DialogTitle>Inactive Device</DialogTitle>
-
-        </CustomDialog>
+        <Snackbar
+            anchorOrigin={{vertical: 'top', horizontal: 'center'}}
+            open={open}
+            autoHideDuration={3000}
+            onClose={onClose}
+            message="Inactive Device"
+        />
     );
 };
 
